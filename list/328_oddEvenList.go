670b119bb9a24348a5210a0fb49de8e18f8692b7
Aeishen <AeishenLin@gmail.com>
2020-11-02 18:28:09 +0800

    update:添加奇偶链表

---
+++ list/328_oddEvenList.go
@@ -0,0 +1,22 @@
+package list
+
+// 奇偶链表
+
+// oddEvenList 双指针
+func oddEvenList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	f,s := head,head.Next
+
+	for cur := s; cur != nil && cur.Next != nil; {
+		f.Next = f.Next.Next
+		cur.Next = cur.Next.Next
+		f = f.Next
+		cur = cur.Next
+	}
+	f.Next = s
+
+	return head
+}
